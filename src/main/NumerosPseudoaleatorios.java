869fc2df29f236966f5e071ac1206220edc4c494
Joan Garcia <joangarcia2015@gmail.com>
2019-03-03 00:20:14 -0600

    Creación de la clase Gui.

--- src/main/NumerosPseudoaleatorios.java
+++ src/main/NumerosPseudoaleatorios.java
@@ -1,11 +1,11 @@
 
 package main;
 
+import view.Gui;
+
 public class NumerosPseudoaleatorios {
     
     public static void main(String[] args) {
-       
-        
+        new Gui().launch();
     }
-    
 }
