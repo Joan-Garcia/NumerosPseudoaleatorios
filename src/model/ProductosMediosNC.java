5a7a0d662e5d344001a8c7177aeb5d4194ce3a23
fabricioolson@gmail.com <>
2019-03-05 00:49:25 -0600

    implementación de dos clases

--- src/model/ProductosMediosNC.java
+++ src/model/ProductosMediosNC.java
@@ -24,12 +24,12 @@ public class ProductosMediosNC {
     private void Calcular() {
 
         ArrayList<Integer> equis = new ArrayList();
-        if (D == Integer.toString(x0).length() && D > 3) {
+        if (D == Integer.toString(x0).length() && D >= 3) {
             for (int i = 0; i < iter; i++) {
                 producto = x0 * x1;
                 x0 = x1;
-                x1 =  siguientenumero(producto,i);
-               
+                x1 = siguientenumero(producto, i);
+
             }
         } else {
             JOptionPane.showMessageDialog(null, "El valor de la semilla debe ser igual al de D");
@@ -40,24 +40,16 @@ public class ProductosMediosNC {
     private int siguientenumero(long producto, int i) {
         String snumero2 = Long.toString(producto);
         int tam2 = snumero2.length();
-        int tam1 = Integer.toString(x0).length();
+        int tam1 = D;
         int primerc = (tam2 - tam1) / 2;
-        if(tam2%2==0){
-        String snumero3 = snumero2.substring(primerc, primerc + tam1);
-      
-        System.out.println(i + ". " + snumero3);
-        int numero1;
-        return numero1 = Integer.parseInt(snumero3);
-  } else{
-             String snumero3 = snumero2.substring(primerc, primerc + tam1+1);
-      
-        System.out.println(i + ". " + snumero3);
-        int numero1;
-        return numero1 = Integer.parseInt(snumero3);
-        }
+            String snumero3 = snumero2.substring(primerc, primerc + tam1);
+            System.out.println(i + ". " + snumero3);
+            int numero1;
+            return numero1 = Integer.parseInt(snumero3);
     }
+    
     public static void main(String[] args) {
-        new ProductosMediosNC(5,90111,11111,10).Calcular();
+        new ProductosMediosNC(3, 418, 115, 10).Calcular();
     }
 
 }
