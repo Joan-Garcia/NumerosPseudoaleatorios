b69759a9c61d8bb6c6bc5191bb9918d61bb7566d
Joan Garcia <joangarcia2015@gmail.com>
2019-03-08 14:18:21 -0600

    Creación de clase que imprime en txt.

--- src/view/Gui.java
+++ src/view/Gui.java
@@ -4,16 +4,24 @@ Autor: Joan Roberto Garcia Garcia
 */
 package view;
 
+import javax.swing.ButtonGroup;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JRadioButton;
 
 public class Gui {
     private final JFrame window;
     private final JPanel panel;
+    private final JRadioButton pmedios, cuadratico, constante;
+    private final ButtonGroup rgroup;
     
     public Gui(){
         window = new JFrame();
         panel = new JPanel();
+        pmedios = new JRadioButton("Productos Medios");
+        cuadratico = new JRadioButton("Cuadratico");
+        constante = new JRadioButton("Constante Multiplicativo");
+        rgroup = new ButtonGroup();
     }
     
     private void windowSettings(){
@@ -22,9 +30,21 @@ public class Gui {
         window.setResizable(false);
     }
     
+    private void rGroupSettings(){
+        rgroup.add(pmedios);
+        rgroup.add(cuadratico);
+        rgroup.add(constante);
+        
+        pmedios.setBounds(20, 20, 150, 20);
+        cuadratico.setBounds(180, 20, 100, 20);
+        constante.setBounds(300, 20, 200, 20);
+    }
+    
     private void panelSettings(){
         panel.setLayout(null);
-        
+        panel.add(pmedios);
+        panel.add(cuadratico);
+        panel.add(constante);
     }
     
     private void show(){
@@ -35,6 +55,7 @@ public class Gui {
     
     public void launch(){
         windowSettings();
+        rGroupSettings();
         panelSettings();
         show();
     }
